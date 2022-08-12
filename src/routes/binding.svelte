57448d7ccebf93991ee8aebcf37b8c97c3a85bcf
Zach Neibaur <zneibaur@protonmail.com>
2022-08-11 20:38:20 -0600

    Initial commit

---
+++ src/routes/binding.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+</script>
+
+<p>
+  If you ever need to a child component to modify state on a parent component check this out:
+
+  Use the property on the component with the bind keyword in front with the state word right afterwords.
+
+  Then any time the child component modifies the bound state, the parent component will reflect those changes as well.
+</p>
+
+<style>
+
+</style>
