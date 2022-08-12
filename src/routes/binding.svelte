9f5c0124dfe71be80ca3f87f97aae1952fa426b4
Zach Neibaur <zneibaur@protonmail.com>
2022-08-11 20:40:36 -0600

    Testing vercel

--- src/routes/binding.svelte
+++ src/routes/binding.svelte
@@ -2,13 +2,11 @@
 
 </script>
 
-<p>
-  If you ever need to a child component to modify state on a parent component check this out:
+<p>If you ever need to a child component to modify state on a parent component check this out:</p>
 
-  Use the property on the component with the bind keyword in front with the state word right afterwords.
+<p>Use the property on the component with the bind keyword in front with the state word right afterwords.</p>
 
-  Then any time the child component modifies the bound state, the parent component will reflect those changes as well.
-</p>
+<p>Then any time the child component modifies the bound state, the parent component will reflect those changes as well.</p>
 
 <style>
 
